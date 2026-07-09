4799630678fa3a92fa47e458b2f50a6e4d12d84f
Ricardo Robles <ricardo.r.f@hotmail.com>
2026-07-09 14:43:11 +0200

    Add .gitignore for Rust build artifacts and temp files

--- target/debug/deps/num_integer-4f1fd7c08ec86f81.d
+++
@@ -1,9 +0,0 @@
-/home/sheik/Documentos/rustpython/target/debug/deps/num_integer-4f1fd7c08ec86f81.d: /home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs /home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs /home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs
-
-/home/sheik/Documentos/rustpython/target/debug/deps/libnum_integer-4f1fd7c08ec86f81.rlib: /home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs /home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs /home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs
-
-/home/sheik/Documentos/rustpython/target/debug/deps/libnum_integer-4f1fd7c08ec86f81.rmeta: /home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs /home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs /home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs
-
-/home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs:
-/home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs:
-/home/sheik/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs:
